018fab37bc38256055de43497fc868c67417552e
AndrehAguiar <andreaguiar.g@gmail.com>
2021-09-07 10:45:10 -0300

    Create detaikls selectors

---
+++ src/app/shared/state/router/router.selectors.ts
@@ -0,0 +1,15 @@
+import { RouterReducerState } from "@ngrx/router-store";
+import { createFeatureSelector, createSelector } from "@ngrx/store";
+import { IRouterState } from "./router.reducer";
+
+const selectRouterReducerState = createFeatureSelector<RouterReducerState<IRouterState>>('router');
+
+export const selectRouterState = createSelector(
+    selectRouterReducerState,
+    (routerReducerState: RouterReducerState<IRouterState>) => (routerReducerState && routerReducerState.state)
+);
+
+export const selectQueryParams = createSelector(
+    selectRouterState,
+    (routerState: IRouterState) => (routerState && routerState.queryParams) || {},
+);
